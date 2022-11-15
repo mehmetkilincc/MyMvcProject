65f0db7ec2946db4632b54cda7e01707bf72d549
Mehmet Kılınç <mehmetkilinc2734@gmail.com>
2022-11-15 10:57:16 +0300

    Create context class

---
+++ MyMvcProject.DataAccess/Concrete/MyMvcProjectContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyMvcProject.Entity.Concrete;
+
+namespace MyMvcProject.DataAccess.Concrete
+{
+    public class MyMvcProjectContext :DbContext
+    {
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Content> Contents { get; set; }
+        public DbSet<Heading> Headings { get; set; }
+        public DbSet<Writer> Writers { get; set; }
+    }
+}
--- MyMvcProject.Entity/Concrete/Contact.cs
+++ MyMvcProject.Entity/Concrete/Contact.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MyMvcProject.Entity
+namespace MyMvcProject.Entity.Concrete
 {
     public class Contact
     {
