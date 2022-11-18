7ea418b6ee4f823ce4c9c3c644ffc0f130ed4437
Mehmet Kılınç <mehmetkilinc2734@gmail.com>
2022-11-18 10:08:12 +0300

    Creation and implementation of the generic IEntityRepository interface

---
+++ MyMvcProject.DataAccess/Abstract/ICategoryDal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyMvcProject.Entity.Concrete;
+
+namespace MyMvcProject.DataAccess.Abstract
+{
+    public interface ICategoryDal:IEntityRepository<Category>
+    {
+    }
+}
---
+++ MyMvcProject.DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMvcProject.DataAccess.Abstract
+{
+    public interface IEntityRepository<TEntity>
+    {
+        List<TEntity> GetAll();
+        List<TEntity> GetAll(Expression<Func<TEntity, bool>> filter);
+        void Add(TEntity entity);
+        void Update(TEntity entity);
+        void Delete(TEntity entity);
+    }
+}
