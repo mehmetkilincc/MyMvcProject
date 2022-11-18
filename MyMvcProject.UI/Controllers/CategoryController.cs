8333485154a0e24585b823cfb518629e51b5f70a
Mehmet Kılınç <mehmetkilinc2734@gmail.com>
2022-11-18 10:54:03 +0300

    Creation of category controller and view

---
+++ MyMvcProject.UI/Controllers/CategoryController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyMvcProject.Business.Abstract;
+using MyMvcProject.Business.Concrete;
+using MyMvcProject.DataAccess.Concrete;
+
+namespace MyMvcProject.UI.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryService _categoryService = new CategoryManager(new CategoryDal());
+        // GET: Category
+        public ActionResult Index()
+        {
+            var categories = _categoryService.GetAll();
+
+            return View(categories);
+        }
+    }
+}
