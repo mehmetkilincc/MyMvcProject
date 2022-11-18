bb16d86f58fd3f82d639a290ecc998d2a940113e
Mehmet Kılınç <mehmetkilinc2734@gmail.com>
2022-11-18 10:38:39 +0300

    Creation of category manager and service

---
+++ MyMvcProject.Business/Abstract/ICategoryService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyMvcProject.Entity.Concrete;
+
+namespace MyMvcProject.Business.Abstract
+{
+    public interface ICategoryService
+    {
+        List<Category> GetAll();
+        void Add(Category category);
+        void Update(Category category);
+        void Delete(Category category);
+
+    }
+}
---
+++ MyMvcProject.Business/Concrete/CategoryManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyMvcProject.Business.Abstract;
+using MyMvcProject.DataAccess.Abstract;
+using MyMvcProject.Entity.Concrete;
+
+namespace MyMvcProject.Business.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        private ICategoryDal _categoryDal;
+
+        public CategoryManager(ICategoryDal categoryDal)
+        {
+            _categoryDal=categoryDal;
+        }
+
+        public List<Category> GetAll()
+        {
+            return _categoryDal.GetAll();
+        }
+
+        public void Add(Category category)
+        {
+            _categoryDal.Add(category);
+        }
+
+        public void Update(Category category)
+        {
+            _categoryDal.Update(category);
+        }
+
+        public void Delete(Category category)
+        {
+            _categoryDal.Delete(category);
+        }
+    }
+}
