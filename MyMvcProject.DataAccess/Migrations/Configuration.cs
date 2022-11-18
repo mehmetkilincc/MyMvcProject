613e790cd7ef900ef0b7ad4e11ec8e3318cb0d6c
Mehmet Kılınç <mehmetkilinc2734@gmail.com>
2022-11-18 09:22:27 +0300

    Database creation and migration

---
+++ MyMvcProject.DataAccess/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace MyMvcProject.DataAccess.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<MyMvcProject.DataAccess.Concrete.MyMvcProjectContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(MyMvcProject.DataAccess.Concrete.MyMvcProjectContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
